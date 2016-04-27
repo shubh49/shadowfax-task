f0c842454f87086646ec99ba22b5f681ef51b214
shubh49 <shubhamccdps@gmail.com>
2016-04-27 15:08:46 +0530

    updated readme. TODO - Handle Abort

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 ## ESPGAME -> Shadowfax-task
-> Problem
->Create an ESP game for Matching images. e.g. Artigo is an esp game that is used for tagging.
+> Problem \
+Create an ESP game for Matching images. e.g. Artigo is an esp game that is used for tagging.
 
-> Problem Description/Rules
->You need atleast two players paired to play this game at a time.
+
+> Problem Description/Rules  
+You need atleast two players paired to play this game at a time.
 Each player may not know whom they are paired with.
 During the game play each paired player is shown the same question.
 Each question would have a primary image and a set of secondary images to match to the primary image
 Both paired players can move to the next question only when they choose the same set of secondary images
+
+## Live Demo
+
+https://afternoon-springs-44315.herokuapp.com/
+
+## To run locally
+
+- npm install
+- node index.js
+- Open http://localhost:3000 in browser
 
 ## Technology
 
 - **Express**   - Server side framework
 - **Socket-io** - Library for implementing sockets
 - **Bootstrap** - Library for responsive design
-
-## Live Demo
-
-https://afternoon-springs-44315.herokuapp.com/
--- index.js
+++ index.js
@@ -5,121 +5,125 @@ var io = require('socket.io')(http);
 var clients = [];
 var userResponse = {};
 var flag = false;
+var client_id;
 // var clients = io.sockets.clients();
 
 app.use("/static", express.static(__dirname+'/static'));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname+'/homepage.html');
+        res.sendFile(__dirname+'/homepage.html');
 });
 
 app.get('/stage*', function(req, res) {
-  res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname+'/index.html');
 })
 
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
     // console.log(clients);
     socket.on('create', function(room) {
-      //console.log(room);
-      socket.join(room);
-      // console.log(room);
-      clients.push(socket.id);
-      // console.log(clients);
-      var rom = io.sockets.adapter.rooms[room];
-      a = Object.keys(rom).length;
-      if(a == 1) {
-        msg = 'You are the first one here. So you would have to wait till you are paired with someone!'
-        socket.emit('roomCreated', msg);
-      }
-      if(a == 2) {
-        msg = 'You have got a pair. Choose your option and proceed.';
-        socket.broadcast.to(clients[0]).emit('gotPair', msg);
-      }
-      if (a > 2) {
-        msg = 'Sorry maximum limit reached. Comeback later!'
-        socket.emit('limitReached', msg);
-        socket.leave(room);
-      }
+        //console.log(room);
+        socket.join(room);
+        // console.log(room);
+        clients.push(socket.id);
+        // console.log(clients);
+        var rom = io.sockets.adapter.rooms[room];
+        a = Object.keys(rom).length;
+        if(a == 1) {
+            msg = 'You are the first one here. So you would have to wait till you are paired with someone!'
+            socket.emit('roomCreated', msg);
+        }
+        if(a == 2) {
+            msg = 'You have got a pair. Choose your option and proceed.';
+            socket.broadcast.to(clients[0]).emit('gotPair', msg);
+            msg2 = 'You are paired with someone. So no need to wait. Enjoy!'
+            socket.emit('gotPair', msg2);
+        }
+        if (a > 2) {
+            msg = 'Sorry maximum limit reached. Comeback later!'
+            socket.emit('limitReached', msg);
+            socket.leave(room);
+        }
     });
 
     socket.on('choice', function(choiceNo) {
-      // console.log(choiceNo);
-      if(!userResponse[choiceNo]) {
-        for (var i = 0; i < 4; i++) {
-          if(userResponse[i] == socket.id) {
-            delete userResponse[i];
-          }
-        }
-        userResponse[choiceNo] = socket.id;
-        // consol e.log(userResponse);
-        if(Object.keys(userResponse).length == 1) {
-          msg = 'Your choice is registered. Waiting for other user response';
-          socket.emit('registered', msg);
-        }
-        else {
-          msg = 'Choices did not match. Try Again.'
-          io.emit('misMatch', msg);
-        }
-      }
-      else {
-        if(userResponse[choiceNo] != socket.id) {
-          msg = 'Choices matched! Enjoy next stage.'
-          io.emit('match', msg);
-          // console.log('success');
+        // console.log(choiceNo);
+        if(!userResponse[choiceNo]) {
+            for (var i = 0; i < 4; i++) {
+                if(userResponse[i] == socket.id) {
+                    delete userResponse[i];
+                }
+            }
+            userResponse[choiceNo] = socket.id;
+            // consol e.log(userResponse);
+            if(Object.keys(userResponse).length == 1) {
+                msg = 'Your choice is registered. Waiting for other user response';
+                socket.emit('registered', msg);
+            }
+            else {
+                msg = 'Choices did not match. Try Again.'
+                io.emit('misMatch', msg);
+            }
         }
         else {
-          msg = 'This choice is already registered by you. Wait for other user.'
-          socket.emit('alreadyRegistered', msg);
+            if(userResponse[choiceNo] != socket.id) {
+                msg = 'Choices matched! Enjoy next stage.'
+                io.emit('match', msg);
+                // console.log('success');
+            }
+            else {
+                msg = 'This choice is already registered by you. Wait for other user.'
+                socket.emit('alreadyRegistered', msg);
+            }
         }
-      }
     });
 
     socket.on('nextStage', function(room) {
-        if (!flag) {
-          userResponse = {};
-          flag = true;
-        }
-        else {
-          flag = false;
-        }
-        io.emit('getNextStage', room);
-    })
-
-    socket.on('disconnet', function() {
-      msg = 'Sad! Your pair disconneted so you will be redirected to homepage.';
-      console.log(msg);
-      for (var i = 0; i < clients.length; i++) {
-        if (clients[i] != socket.id) {
-          client_id = clients[i];
-          break;
-        }
-      }
-      socket.broadcast.to(client_id).emit('disconneted', msg);
+            if (!flag) {
+                userResponse = {};
+                flag = true;
+            }
+            else {
+                flag = false;
+            }
+            io.emit('getNextStage', room);
     });
 
-  //   socket.onclose = function() {
-  //   var client_id;
-  //   msg = 'Sad! Your pair disconneted so you will be redirected to homepage.';
-  //   console.log(msg);
-  //   for (var i = 0; i < clients.length; i++) {
-  //     if (clients[i] != socket.id) {
-  //       client_id = clients[i];
-  //       break;
-  //     }
-  //   }
-  //   socket.broadcast.to(client_id).emit('disconneted', msg);
-  // };
+    socket.on('disconnect', function() {
+        console.log('disconnet');
+        msg = 'Sad! Your pair disconneted so you will be redirected to homepage.';
+        console.log(msg);
+        for (var i = 0; i < clients.length; i++) {
+          if (clients[i] != socket.id) {
+            client_id = clients[i];
+            break;
+          }
+        }
+        socket.broadcast.to(client_id).emit('disconnected', msg);
+    });
+
+    //   socket.onclose = function() {
+    //   var client_id;
+    //   msg = 'Sad! Your pair disconneted so you will be redirected to homepage.';
+    //   console.log(msg);
+    //   for (var i = 0; i < clients.length; i++) {
+    //     if (clients[i] != socket.id) {
+    //       client_id = clients[i];
+    //       break;
+    //     }
+    //   }
+    //   socket.broadcast.to(client_id).emit('disconneted', msg);
+    // };
 
     socket.on('join', function(room) {
-      console.log(room);
-      io.emit('join', room);
-    })
+        console.log(room);
+        io.emit('join', room);
+    });
 
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+            io.emit('chat message', msg);
     });
 });
 
 http.listen(process.env.PORT || 3000,function(){
-    console.log('listening on *:3000');
+        console.log('listening on *:3000');
 });
